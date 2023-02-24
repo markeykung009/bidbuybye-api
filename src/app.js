6fef25263b0025e057c65f00e1c02e6ec9d17142
DESKTOP-9MK91S8\admin <harukasenpais@gmail.com>
2023-02-24 21:22:21 +0700

    modify-api-categoy

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const authenticate = require('./middlewares/authenticate');
 const checkoutRoutes = require('./routes/checkoutRoutes');
 const authRoute = require('./routes/auth-route');
 
-const productRoute = require('./routes/product-route');
+// const productDetailRoute = require('./routes/product-route');
 
 const productRoute = require('./routes/productRoute');
 const categorytRoute = require('./routes/categoryRoute');
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use('/product', productRoute);
 app.use('/category', categorytRoute);
 app.use('/brand', brandRoute);
-app.use('/size', productRoute);
+// app.use('/size', productDetailRoute);
 // app.use('/checkout', authenticate, checkoutRoutes);
 app.use('/checkout', checkoutRoutes);
 app.use('/auth', authRoute);
