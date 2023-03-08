a2021fa304efe6485a4d5a66387a884314c399cc
Pom2022 <nuttakarnpom@gmail.com>
2023-03-08 18:28:49 +0700

    finalpayment

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -12,7 +12,7 @@ exports.getProduct = async (req, res, next) => {
     const products = await Product.findAll({
       include: [{ model: Category }, { model: Brand }]
     });
-    console.log(products, 'products');
+
     res.status(201).json({ products });
   } catch (err) {
     next(err);
